24dd2f6e4f789db0bb5448557b8db92b7c3c488c
username <wjdgur12345@gmail.com>
2024-10-23 23:20:34 +0900

    강제로 1스테이지로 이동하는 개발자기능 추가

---
+++ Assets/Scripts/Util/DevelopOnlyRestart.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevelopOnlyRestart : MonoBehaviour
+{
+    private RectTransform _rectTransform = null;
+
+    private float _minX, _minY;
+    private float _maxX, _maxY;
+
+    [SerializeField, Range(1f, 10f)] private float restartTime = 2f; 
+
+    private void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+
+        _minX = _rectTransform.position.x - (_rectTransform.sizeDelta.x / 2f);
+        _maxX = _rectTransform.position.x + (_rectTransform.sizeDelta.x / 2f);
+
+        _minY = _rectTransform.position.y - (_rectTransform.sizeDelta.y / 2f);
+        _maxY = _rectTransform.position.y + (_rectTransform.sizeDelta.y / 2f);
+    }
+
+    private float _pressTime = -1f;
+    
+    private void Update()
+    {
+        if (Input.touchCount <= 0) return;
+
+        Touch t = Input.GetTouch(0);
+
+        // 터치 상태 확인
+        if (t.phase == TouchPhase.Ended)
+        {
+            _pressTime = -1f;
+        }
+
+        else if (_pressTime < 0 && t.phase == TouchPhase.Began && CheckClick(t.position))
+        {
+            _pressTime = Time.time;
+            //Debug.Log("press");
+        }
+
+        // debug
+        //else
+        //{
+        //    Debug.Log($"{Time.time} // {_pressTime + restartTime}");
+        //}
+
+        // 일정시간동안 누르는 경우
+        if (_pressTime > 0f && _pressTime + restartTime <= Time.time)
+        {
+            _pressTime = -1f;
+
+            Destroy(this); // 스크립트만 제거
+            UnityEngine.SceneManagement.SceneManager.LoadScene("First_GameScene");
+        }
+    }
+
+    private bool CheckClick(Vector2 touch)
+    {
+        if (touch.x < _minX || touch.x > _maxX) return false;
+        if (touch.y < _minY || touch.y > _maxY) return false;
+
+        return true;
+    }
+}
